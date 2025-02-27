0e8bf1f9b04d7bb2e434c062f52e595dcce9fbc1
saeldean <sdean@unc.edu>
2025-02-27 16:27:42 -0500

    update hiker geojson

--- Homeworks/Homework5/hikingData4.js
+++ Homeworks/Homework5/hikingData4.js
@@ -2,15 +2,15 @@ var data = {
     "type": "FeatureCollection",
     "name": "hikingData4",
     "features": [
-    { "type": "Feature", "properties": { "date": "6/12/21", "day": 1, "shelter": "Curley Maple Gap Shelter", "coordinates": [-82.396844, 36.104329] }, "geometry": null },
-    { "type": "Feature", "properties": { "date": "6/13/21", "day": 2, "shelter": "No Buisiness Knob Shelter", "coordinates": [-82.433654, 36.066622] }, "geometry": null },
-    { "type": "Feature", "properties": { "date": "6/14/21", "day": 3, "shelter": "Bald Mountain Shelter", "coordinates": [-82.47808, 36.00016] }, "geometry": null },
-    { "type": "Feature", "properties": { "date": "6/15/21", "day": 4, "shelter": "Hog Back Ridge Shelter Side Trail", "coordinates": [-82.58713, 35.96408] }, "geometry": null },
-    { "type": "Feature", "properties": { "date": "6/16/21", "day": 5, "shelter": "Flint mountain shelter", "coordinates": [82.596637, 36.034858] }, "geometry": null },
-    { "type": "Feature", "properties": { "date": "6/17/21", "day": 6, "shelter": "Little Laurel Shelter", "coordinates": [-82.735924, 36.015103] }, "geometry": null },
-    { "type": "Feature", "properties": { "date": "6/18/21", "day": 7, "shelter": "Rich Mountain Campsite", "coordinates": [-82.791154, 35.910422] }, "geometry": null },
-    { "type": "Feature", "properties": { "date": "6/19/21", "day": 8, "shelter": "Deer Park Mountain Shelter", "coordinates": [-82.861248, 35.875954] }, "geometry": null },
-    { "type": "Feature", "properties": { "date": "6/20/21", "day": 9, "shelter": "Walnut Mountain Shelter", "coordinates": [-82.93649, 35.836503] }, "geometry": null },
-    { "type": "Feature", "properties": { "date": "6/21/21", "day": 10, "shelter": "Max Patch", "coordinates": [-82.9568, 35.797] }, "geometry": null }
+    { "type": "features", "properties": { "date": "6/12/21", "day": 1, "shelter": "Curley Maple Gap Shelter", "coordinates": [-82.396844, 36.104329] }, "geometry": null },
+    { "type": "features", "properties": { "date": "6/13/21", "day": 2, "shelter": "No Buisiness Knob Shelter", "coordinates": [-82.433654, 36.066622] }, "geometry": null },
+    { "type": "features", "properties": { "date": "6/14/21", "day": 3, "shelter": "Bald Mountain Shelter", "coordinates": [-82.47808, 36.00016] }, "geometry": null },
+    { "type": "features", "properties": { "date": "6/15/21", "day": 4, "shelter": "Hog Back Ridge Shelter Side Trail", "coordinates": [-82.58713, 35.96408] }, "geometry": null },
+    { "type": "features", "properties": { "date": "6/16/21", "day": 5, "shelter": "Flint mountain shelter", "coordinates": [82.596637, 36.034858] }, "geometry": null },
+    { "type": "features", "properties": { "date": "6/17/21", "day": 6, "shelter": "Little Laurel Shelter", "coordinates": [-82.735924, 36.015103] }, "geometry": null },
+    { "type": "features", "properties": { "date": "6/18/21", "day": 7, "shelter": "Rich Mountain Campsite", "coordinates": [-82.791154, 35.910422] }, "geometry": null },
+    { "type": "features", "properties": { "date": "6/19/21", "day": 8, "shelter": "Deer Park Mountain Shelter", "coordinates": [-82.861248, 35.875954] }, "geometry": null },
+    { "type": "features", "properties": { "date": "6/20/21", "day": 9, "shelter": "Walnut Mountain Shelter", "coordinates": [-82.93649, 35.836503] }, "geometry": null },
+    { "type": "features", "properties": { "date": "6/21/21", "day": 10, "shelter": "Max Patch", "coordinates": [-82.9568, 35.797] }, "geometry": null }
     ]
     }
